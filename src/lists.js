8c5153563a432bb101403ff170daadf51c9b3f43
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-10-08 16:57:53 +0100

    Store value of user input for newLists

--- src/lists.js
+++ src/lists.js
@@ -1,2 +1,10 @@
 let toDoLists = [];
 let currentList = null;
+
+const lists = document.getElementById('newList').addEventListener('click', (event) => {
+    const listValue = lists.value;
+    toDoLists.push(listValue);
+    console.log(toDoLists);
+})
+
+export { toDoLists, currentList, lists };
