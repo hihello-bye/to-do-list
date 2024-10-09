abe188a6f84dddfc0a6a179cf682ba3e490eb02d
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-10-09 16:51:03 +0100

    Add task and update D0M

--- src/lists.js
+++ src/lists.js
@@ -6,6 +6,9 @@ let currentList = null;
 const newList = document.getElementById('newList');
 const createList = document.getElementById('createListBtn');
 const lists = document.getElementById('lists');
+const newTask = document.getElementById('newTask');
+const createTask = document.getElementById('addTaskBtn');
+const tasks = document.getElementById('tasks');
 
 createList.addEventListener('click', (event) => {
     const listValue = newList.value;
@@ -22,6 +25,24 @@ createList.addEventListener('click', (event) => {
         })
 
         newList.value='';
+    } 
+})
+
+createTask.addEventListener('click', (event) => {
+    const taskValue = newTask.value;
+
+    if (taskValue) {
+        toDoLists.push({task: taskValue});
+        
+        const addTask = document.createElement('button');
+        addTask.textContent = taskValue;
+        tasks.appendChild(addTask);
+
+        addTask.addEventListener('click', (event) => {
+            currentList = toDoLists.find(tasks => tasks.task === taskValue);
+        })
+
+        newTask.value='';
     } 
 })
 
