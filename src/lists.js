c9c48d4fddf3f59dcf60656f99bcafdd47838320
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-10-09 15:01:21 +0100

    Create button for switching between lists

--- src/lists.js
+++ src/lists.js
@@ -5,18 +5,24 @@ let currentList = null;
 
 const newList = document.getElementById('newList');
 const createList = document.getElementById('createListBtn');
+const lists = document.getElementById('lists');
 
 createList.addEventListener('click', (event) => {
     const listValue = newList.value;
 
     if (listValue) {
-        toDoLists.push(listValue);
-        console.log(toDoLists);
+        toDoLists.push({name: listValue});
+
+        const switchList = document.createElement('button');
+        switchList.textContent = listValue;
+        lists.appendChild(switchList);
+
+        switchList.addEventListener('click', (event) => {
+            currentList = toDoLists.find(list => list.name === listValue);
+        })
 
         newList.value='';
-    } else {
-        console.log('Please name me');
-    }
+    } 
 })
 
 };
