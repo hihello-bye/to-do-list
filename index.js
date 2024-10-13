7563e2311a8129b9e4acc1ef8cba3c8d4a0ee773
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-10-13 13:25:16 +0100

    Get references to dom elements

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 let toDoList = [];
 let currentList = null;
+
+const listContainer = document.getElementById('listContainer');
+const taskContainer = document.getElementById('taskContainer');
+const newListInput = document.getElementById('newListInput');
+const createListBtn = document.getElementById('createListBtn');
+const newTaskInput = document.getElementById('newTaskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
