4d369a43e5b4e030cbc6858c4c9cce3815548720
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-10-16 16:39:20 +0100

    load from local storage

--- src/storage.js
+++ src/storage.js
@@ -1,3 +1,12 @@
 export function saveToLocalStorage(data) {
     localStorage.setItem('toDoList', JSON.stringify(data));
 }
+
+export function loadLocalStorage() {
+    const storedList = localStorage.getItem('toDolist');
+    
+    if (storedList) {
+        return JSON.parse(storedList);
+    }
+    return null
+}
